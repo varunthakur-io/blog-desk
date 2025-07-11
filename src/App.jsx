f31fdb824c223288569db8818662bb29bf9dba29
Varun Thakur <varunkumar84150@gmail.com>
2025-07-12 01:46:09 +0530

    feat: add session check using account.get() to persist user on refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
 import './App.css';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setUser, clearUser } from './store/authSlice';
+import { authService } from './services/authService';
+
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
-import { Routes, Route } from 'react-router';
-import Navbar from './components/Navbar';
+import CreatePost from './pages/CreatePost';
 import Home from './pages/Home';
 
+import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const checkUser = async () => {
+      try {
+        const user = await authService.getAccount();
+        dispatch(setUser(user));
+      } catch (error) {
+        dispatch(clearUser());
+        console.error('No user found, clearing user state:', error.message);
+      }
+    };
+
+    checkUser();
+  }, [dispatch]);
+
   return (
     <>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/create" element={<CreatePost />} />
       </Routes>
     </>
   );
