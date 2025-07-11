c9065f71f3c986093622ee64a5fa4215d939e862
Varun Thakur <varunkumar84150@gmail.com>
2025-07-11 15:55:59 +0530

    feat: set up React Router with login and signup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import { Routes, Route } from 'react-router';
 
 function App() {
   return (
-    <>
-      <Signup />
-      <Login />
-    </>
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+
+      <Route
+        path="*"
+        element={<h1 className="text-center mt-10">404 Page Not Found</h1>}
+      />
+    </Routes>
   );
 }
 
