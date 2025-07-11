a3b6fbb9082b4097c0ac8efc32f8821a63df1191
Varun Thakur <varunkumar84150@gmail.com>
2025-07-11 14:11:38 +0530

    chore: install and configure Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Login from './components/Login';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Login></Login>
+    </>
+  );
 }
 
 export default App;
