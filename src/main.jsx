f5ecf7213ba7c390829ea3bb57af96e5a2526b4d
Varun Thakur <varunkumar84150@gmail.com>
2025-07-19 01:31:10 +0530

    fix: resolve login flash and redirect to / on refresh with Root.jsx auth check

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import Root from './Root.jsx';
 import './index.css';
 
-import { BrowserRouter } from 'react-router';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </StrictMode>
-  </Provider>
+  <StrictMode>
+    <Root />
+  </StrictMode>
 );
