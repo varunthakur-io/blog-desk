f98235d93b301197bc11dc8c082860a4a47b8383
Varun Thakur <varunkumar84150@gmail.com>
2025-11-24 16:36:11 +0530

    chore: integrate profileSlice into Redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
 import postReducer from './postSlice';
+import profileReducer from './profileSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     posts: postReducer,
+    profile: profileReducer,
   },
 });
