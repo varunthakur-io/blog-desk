f562352a0049458734a81a74df6d73f613d81573
Varun Thakur <varunkumar84150@gmail.com>
2025-12-07 16:43:25 +0530

    chore: update appwrite and .env variables

--- src/config/appwrite.js
+++ src/config/appwrite.js
@@ -3,7 +3,7 @@ export const appwriteConfig = {
   url: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
   projectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
   databaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-  postsCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  postsCollectionId: String(import.meta.env.VITE_APPWRITE_POSTS_COLLECTION_ID),
   likesCollectionId: String(import.meta.env.VITE_APPWRITE_LIKES_COLLECTION_ID),
   commentsCollectionId: String(import.meta.env.VITE_APPWRITE_COMMENTS_COLLECTION_ID),
   bucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
