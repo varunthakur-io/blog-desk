fb8def32e063faffeec2fc2248df7bb2ff5659a1
Varun Thakur <varunkumar84150@gmail.com>
2025-12-08 14:57:27 +0530

    feat(utils): add debounce utility function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,15 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export function debounce(func, wait) {
+  let timeout;
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+}
