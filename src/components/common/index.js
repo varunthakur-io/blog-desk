5d1407e7a566ac5c786d7865fcddbac937da013c
Varun Thakur <varunkumar84150@gmail.com>
2026-03-13 20:26:18 +0530

    chore: remove unused file

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,4 +1,3 @@
 export { default as Navbar } from './Navbar';
 export { default as PrivateRoute } from './PrivateRoute';
-export { default as ShadcnTest } from './ShadcnTest';
 export { default as AppSkeleton } from './AppSkeleton';
