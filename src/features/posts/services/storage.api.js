252ebfb617a9c58a63f24aced6426a549afd728a
Varun Thakur <varunkumar84150@gmail.com>
2026-03-29 19:11:01 +0530

    fix(arch): restore and migrate storage service to posts feature

---
+++ src/features/posts/services/storage.api.js
@@ -0,0 +1,19 @@
+import { storage, appwriteConfig as appwrite } from '@/lib/appwrite';
+import { ID } from 'appwrite';
+
+class StorageApi {
+  async createFile(file) {
+    return await storage.createFile(appwrite.bucketId, ID.unique(), file);
+  }
+
+  async deleteFile(fileId) {
+    return await storage.deleteFile(appwrite.bucketId, fileId);
+  }
+
+  getFileViewUrl(fileId) {
+    // Build a stable public view URL once so callers do not repeat Appwrite URL assembly.
+    return `${appwrite.url}/storage/buckets/${appwrite.bucketId}/files/${fileId}/view?project=${appwrite.projectId}`;
+  }
+}
+
+export const storageApi = new StorageApi();
