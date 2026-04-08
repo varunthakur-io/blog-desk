1f58280cf240a1e26fff5c894c712cff82caf270
Varun Thakur <varunkumar84150@gmail.com>
2026-04-09 01:00:34 +0530

    feat(seo): install react-helmet-async and configure HelmetProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,19 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { HelmetProvider } from 'react-helmet-async';
 import { store } from './store/store';
 import App from './App.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </HelmetProvider>
   </StrictMode>,
 );
