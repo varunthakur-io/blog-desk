40dd0e7bd0d38c95c600451d8cd3bda8665a3382
Varun Thakur <varunkumar84150@gmail.com>
2026-04-12 00:57:21 +0530

    ui(profile): implement NetworkDialog for sleek on-demand connection views

--- src/features/profile/components/index.js
+++ src/features/profile/components/index.js
@@ -1,3 +1,4 @@
 export { default as EditProfileDialog } from './EditProfileDialog';
 export { default as ProfileTabs } from './ProfileTabs';
+export { default as NetworkDialog } from './NetworkDialog';
 export * from './ProfileUI';
