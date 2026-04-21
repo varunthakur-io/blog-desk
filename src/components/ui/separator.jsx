da7ed06967a8c0ed78b196b5ec95d45d48ea0e75
Varun Thakur <varunkumar84150@gmail.com>
2026-04-21 10:37:17 +0530

    refactor: update shadcn components for tailwind v4 compatibility and modern api

--- src/components/ui/separator.jsx
+++ src/components/ui/separator.jsx
@@ -1,26 +1,27 @@
-import * as React from 'react';
-import * as SeparatorPrimitive from '@radix-ui/react-separator';
+"use client"
 
-import { cn } from '@/lib/utils';
+import * as React from "react"
+import { Separator as SeparatorPrimitive } from "radix-ui"
 
-const Separator = React.forwardRef(
-  (
-    { className, orientation = 'horizontal', decorative = true, ...props },
-    ref,
-  ) => (
+import { cn } from "@/lib/utils"
+
+function Separator({
+  className,
+  orientation = "horizontal",
+  decorative = true,
+  ...props
+}) {
+  return (
     <SeparatorPrimitive.Root
-      ref={ref}
+      data-slot="separator"
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        'shrink-0 bg-border',
-        orientation === 'horizontal' ? 'h-[1px] w-full' : 'h-full w-[1px]',
-        className,
+        "shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+        className
       )}
-      {...props}
-    />
-  ),
-);
-Separator.displayName = SeparatorPrimitive.Root.displayName;
+      {...props} />
+  );
+}
 
-export { Separator };
+export { Separator }
